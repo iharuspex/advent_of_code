78c9f7225a5b180fce15f0642c9cfebf4adcd10b
aparaev <nabrosok-hd@yandex.ru>
2021-12-02 14:31:46 +0700

    add day2 part2

---
+++ day2_dive/part2/src/main.adb
@@ -0,0 +1,43 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure Main is
+   type Commands_Type is (forward, down, up);
+   package Commands_IO is new Ada.Text_IO.Enumeration_IO (Commands_Type);
+
+   Input_File     : File_Type;
+
+   Command        : Commands_Type;
+   Value          : Integer;
+
+   Horizontal_Pos : Integer := 0;
+   Depth          : Integer := 0;
+   Aim            : Integer := 0;
+
+begin
+   Open (File => Input_File, Mode => In_File, Name => "input.txt");
+
+   while not End_Of_File (Input_File) loop
+      Commands_IO.Get (File => Input_File, Item => Command);
+      Get (File => Input_File, Item => Value);
+
+      --  Commands_IO.Put (Command);
+      --  Put (Item => Value, Width => 1);
+      --  New_Line;
+
+      case Command is
+         when forward =>
+            Horizontal_Pos := Horizontal_Pos + Value;
+            Depth := Depth + Aim * Value;
+         when down =>
+            Aim := Aim + Value;
+         when up =>
+            Aim := Aim - Value;
+      end case;
+
+   end loop;
+
+   Close (Input_File);
+
+   Put (Horizontal_Pos * Depth);
+end Main;
