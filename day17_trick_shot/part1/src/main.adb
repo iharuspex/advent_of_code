997d2b2edda8d290ad73966debe891e4d2d0611d
aparaev <nabrosok-hd@yandex.ru>
2021-12-17 16:23:42 +0700

    wip day17

--- day17_trick_shot/part1/src/main.adb
+++ day17_trick_shot/part1/src/main.adb
@@ -4,17 +4,30 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Main is
    Input_File : File_Type;
 
+   -- type Diagram_Type is array (Integer range <>, Integer range <>) of Character;
+
    subtype Target_X is Integer range 20 .. 30;
    subtype Target_Y is Integer range -10 .. -5;
 
-   Vx : Integer := 17;
-   Vy : Integer := -4;
+   Vx : Integer := 7;
+   Vy : Integer := 10;
 
    Pos_x : Integer := 0;
    Pos_y : Integer := 0;
 
+   -- Diagram : Diagram_Type (-50 .. 50, -50 .. 50) := (others => (others => '.'));
+
 begin
    Open (Input_File, In_File, "input.txt");
+
+   --  Diagram (0, 0) := 'S';
+   --
+   --  for I in Target_Y'Range loop
+   --     for J in Target_X'Range loop
+   --        Diagram (I, J) := 'T';
+   --     end loop;
+   --  end loop;
+
 
    loop
       Pos_x := Pos_x + Vx;
@@ -28,6 +41,7 @@ begin
 
       Put_Line ("X = " & Pos_x'Image & ", Y = " & Pos_y'Image);
       Put_Line ("Vx = " & Vx'Image & ", Vy = " & Vy'Image);
+      --  Diagram (Pos_y, Pos_x) := '#';
       New_Line;
 
       if Pos_x in Target_X'Range and Pos_y in Target_Y'Range then
@@ -36,6 +50,13 @@ begin
          exit;
       end if;
    end loop;
+
+   --  for I in reverse Diagram'Range (1) loop
+   --     for J in Diagram'Range (2) loop
+   --        Put (Diagram (I, J) & " ");
+   --     end loop;
+   --     New_Line;
+   --  end loop;
 
    Close (Input_File);
 
