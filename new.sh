d5681f53bf7696d06b82ee124b71952406748e6c
aparaev <nabrosok-hd@yandex.ru>
2022-12-02 14:42:26 +0700

    improve new.sh script

--- new.sh
+++ new.sh
@@ -7,6 +7,17 @@ help()
     exit 1
 }
 
+setup_vscode_project()
+{
+    if [ ! -d ".vscode" ]
+    then
+        mkdir .vscode
+    fi
+
+    cp template/settings_template.json .vscode/settings.json
+    sed -i "s#gpr_file_path#$1#" .vscode/settings.json
+}
+
 while getopts "n:p:" opt
 do
     case "$opt" in
@@ -26,17 +37,38 @@ fi
 if [ ! -d "$AOC_FOLDER" ]
 then
     mkdir "$AOC_FOLDER"
+    echo "Create new AOC directory..."
 fi
 
 NEW_PROJECT_PATH="$AOC_FOLDER"/"$PROJECT_NAME"
+GPR_FILE_PATH="$NEW_PROJECT_PATH"/"$PROJECT_NAME".gpr
 
 if [ ! -d "$NEW_PROJECT_PATH" ]
 then
-    cp -r template "$NEW_PROJECT_PATH"
+    cp -r template/project "$NEW_PROJECT_PATH"
+else
+    echo "Project $NEW_PROJECT_PATH already exists..."
+    exit 1
 fi
 
+
 NEW_PROJECT_NAME=$(echo "$PROJECT_NAME" | sed -r 's/(^|_)([a-z])/\U_\2/g' | sed -r 's/^.//')
+echo "Preparing the project ${NEW_PROJECT_NAME}..."
 
+echo "Renaming GPR project..."
 sed -i "s/Template/$NEW_PROJECT_NAME/" "$NEW_PROJECT_PATH"/template.gpr
+mv "$NEW_PROJECT_PATH"/template.gpr "$GPR_FILE_PATH"
 
-mv "$NEW_PROJECT_PATH"/template.gpr "$NEW_PROJECT_PATH"/"$PROJECT_NAME".gpr
+echo "Done!"
+
+while true; 
+do
+    read -rp "Setup VSCode Ada extension for new project? [y/n] " yn
+    case $yn in
+        [Yy]* ) setup_vscode_project "$GPR_FILE_PATH"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer [y]es or [n]o.";;
+    esac
+done
+
+echo "Ready to code now! =)"
